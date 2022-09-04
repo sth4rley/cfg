b7ad6208f55cb5e55906f7dbed25ce103b645bc2
Stharley <sth4rley@gmail.com>
2022-09-04 13:31:34 -0300

    bspwm

---
+++ bspwm/autostart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+$HOME/.config/polybar/launch.sh &
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+run sxhkd -c ~/.config/bspwm/sxhkdrc &
+xsetroot -cursor_name left_ptr &
+run nm-applet &
+blueberry-tray &
+picom --config $HOME/.config/bspwm/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+nitrogen --restore &
