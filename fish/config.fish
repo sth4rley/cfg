49a45a29304b0f2baf1d737e77d8525ba982db39
Stharley <sstharley@gmail.com>
2025-09-17 22:32:33 -0300

    17/09/2025 - 2

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function nvd
+    neovide $argv >/dev/null 2>&1 &
+end
+
 set -g fish_greeting ""
 fish_add_path ~/.local/bin
+
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+pyenv init - | source
